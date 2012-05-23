8612717d638c6146eb7ae7839b2c2f5f23338a0c
William Martin <will.st4@gmail.com>
2012-05-23 18:23:41 +0100

    Added build script. #1
    Added one file each for each module and binary, and defined empty methods.

---
+++ include/diffr/suffixtree/SuffixTrees.h
@@ -0,0 +1,30 @@
+/**
+ * @file SuffixTrees.h
+ * @author  William Martin <will.st4@gmail.com>
+ * @since 0.0
+ *
+ * @section LICENSE
+ *
+ * This file is part of diffr-c.
+ * 
+ * diffr-c is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * diffr-c is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with diffr-c.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+#ifndef SUFFIXTREES_H
+#define	SUFFIXTREES_H
+
+void createTree();
+
+#endif	/* SUFFIXTREES_H */
