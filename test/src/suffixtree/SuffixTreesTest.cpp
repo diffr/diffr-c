34dca48285bf71ad4faade68d5132faa14a04092
William Martin <will.st4@gmail.com>
2013-02-07 19:39:29 +0000

    Documentation corrections.

--- test/src/suffixtree/SuffixTreesTest.cpp
+++ test/src/suffixtree/SuffixTreesTest.cpp
@@ -5,20 +5,20 @@
  *
  * @section LICENSE
  *
- * This file is part of tmte-cpp.
+ * This file is part of diffr-c.
  * 
- * tmte-cpp is free software: you can redistribute it and/or modify
+ * diffr-c is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
 
- * tmte-cpp is distributed in the hope that it will be useful,
+ * diffr-c is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
 
  * You should have received a copy of the GNU General Public License
- * along with tmte-cpp.  If not, see <http://www.gnu.org/licenses/>.
+ * along with diffr-c.  If not, see <http://www.gnu.org/licenses/>.
  * 
  */
 
